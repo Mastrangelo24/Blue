7e6e11bf6344626ca3fbe0a95df6d7db767e65df
mastra24 <mastra2410@gmail.com>
2020-10-08 18:46:13 -0300

    ultima section css+html+js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
-
-        var indexValue = 0;
-        function slideShow(){
-          setTimeout(slideShow,  2500);
-          var x;
-          const img = document.querySelectorAll("img");
-          for(x = 0; x < img.length; x++){
-            img[x].style.display = "none";
-            
-          }
-          indexValue++;
-          if(indexValue > img.length){indexValue = 1}
-          img[indexValue -1].style.display = "block";
-        }
-        slideShow();
-
-      
+VanillaTilt.init(document.querySelector(".caja"), {
+  max: 25,
+  speed: 800,
+  perspective: 100,
+}
